f6b521f9b0ab6b18646ed12c9b8a03b06487dfee
Diogo de Lima <diligasi@gmail.com>
2022-03-16 16:58:21 -0300

    Add field forms' search filter

---
+++ app/controllers/admin/field_forms/search_filters_controller.rb
@@ -0,0 +1,30 @@
+module Admin::FieldForms
+  class SearchFiltersController < Admin::AdminController
+
+    def index
+      query = if current_admin_user.admin?
+                FieldForm.includes(:test_tubes, user: [region: [:department]]).where(nil)
+              else
+                FieldForm.includes(:test_tubes, user: [region: [:department]]).where(users: { regions: { department: current_admin_user.region&.department } })
+              end
+
+      query = query.by_agent_name(params[:by_agent_name]) if params[:by_agent_name].present?
+      query = query.by_address(params[:by_address]) if params[:by_address].present?
+
+      if params[:by_date_range].present?
+        range = params[:by_date_range].split(' - ').map { |dt| Date.strptime(dt, '%d/%m/%Y') }
+        query = query.by_date_range(range)
+      end
+
+      query = query.by_field_form_status(params[:by_field_form_status]) if params[:by_field_form_status].present?
+      query = query.by_property_type(params[:by_property_type]) if params[:by_property_type].present?
+      query = query.by_shed_type(params[:by_shed_type]) if params[:by_shed_type].present?
+      query = query.by_larva_species(params[:by_larva_specy]) if params[:by_larva_specy].present?
+      query = query.by_larvae_amount(params[:by_larvae_amount]) if params[:by_larvae_amount].present?
+
+      @field_forms = query.order('departments.id desc, field_forms.created_at, field_forms.status').page(page).per(per_page)
+
+      render 'admin/field_forms/index'
+    end
+  end
+end
