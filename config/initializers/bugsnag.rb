54c17f80c963b1f7b806212c27a5eb70c3fe5e6b
Diogo de Lima <diligasi@gmail.com>
2022-03-20 19:22:05 -0300

    Add heroku version to bugsnag hook

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,5 +1,6 @@
 if Rails.env.production? || Rails.env.staging?
   Bugsnag.configure do |config|
     config.api_key = ENV['BUGSNAG_API_KEY']
+    config.app_version = ENV['HEROKU_RELEASE_VERSION']
   end
 end
